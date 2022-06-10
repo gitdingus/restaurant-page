6bfcd5f9778148b60ff8e03d4ba1ad78b8371071
Josh S <aaabccc87@protonmail.com>
2022-06-09 21:04:47 -0400

    Create nav bar contents in JS

--- src/index.js
+++ src/index.js
@@ -1,1 +1,38 @@
-console.log("Test");
+const content = document.querySelector('#content');
+
+content.appendChild(createNavBar());
+ 
+function createNavBar(){
+    const nav = document.createElement('nav');
+    const div = document.createElement('div');
+    const header = document.createElement('h1');
+    const list = document.createElement('ul');
+    const links = ['Home', 'Menu', 'Reservation', 'Contact'];
+
+    header.textContent = "Theodore Bee's";
+
+    links.forEach(link => list.appendChild(createLinkListItem(link)));
+    
+    div.appendChild(header);
+    div.appendChild(list);
+
+    nav.appendChild(div);
+
+    return nav;
+}
+
+function createLinkListItem(name){
+    const item = document.createElement('li');
+    const link = document.createElement('a');
+
+    link.textContent = name;
+
+    item.appendChild(link);
+
+    return item;
+}
+
+function createFooter(){
+    const footer = document.createElement('footer');
+
+}
