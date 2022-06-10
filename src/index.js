e9644233b7a1e3cedc2814a5634950798a3279f9
Josh S <aaabccc87@protonmail.com>
2022-06-10 10:43:07 -0400

    Add hours to footer

--- src/index.js
+++ src/index.js
@@ -47,13 +47,18 @@ function createFooter(){
     const footer = document.createElement('footer');
     const addressDiv = document.createElement('div');
     const phoneNumberDiv = document.createElement('div');
+    const hoursDiv = document.createElement('div');
     const addressLabel = document.createElement('p');
     const phoneLabel = document.createElement('p');
+    const hoursLabel = document.createElement('p');
+    const monThruFriday = document.createElement('p');
+    const saturday = document.createElement('p');
+    const sunday = document.createElement('p');
     const address = document.createElement('p');
     const cityStateZip = document.createElement('p');
     const phoneNumber = document.createElement('p');
 
-    addressLabel.textContent = 'Address:';
+    addressLabel.textContent = 'Address';
     address.textContent = '1800 Amity Road.';
     cityStateZip.textContent = 'Bethany, CT 06524';
 
@@ -61,13 +66,28 @@ function createFooter(){
     addressDiv.appendChild(address);
     addressDiv.appendChild(cityStateZip);
 
-    phoneLabel.textContent = 'Phone:';
+    hoursLabel.textContent = 'Hours';
+    monThruFriday.textContent = 'Monday through Friday: 9 a.m. to 9 p.m.';
+    saturday.textContent = 'Saturday: 9 a.m. to 9 p.m.';
+    sunday.textContent = 'Sunday: Closed';
+    
+    hoursDiv.appendChild(hoursLabel);
+    hoursDiv.appendChild(monThruFriday);
+    hoursDiv.appendChild(saturday);
+    hoursDiv.appendChild(sunday);
+
+    phoneLabel.textContent = 'Phone';
     phoneNumber.textContent = '(203) 867-5309';
 
     phoneNumberDiv.appendChild(phoneLabel);
     phoneNumberDiv.appendChild(phoneNumber);
 
+    addressDiv.classList.add("address");
+    hoursDiv.classList.add('hours');
+    phoneNumberDiv.classList.add("phone");
+
     footer.appendChild(addressDiv);
+    footer.appendChild(hoursDiv);
     footer.appendChild(phoneNumberDiv);
 
     return footer;
